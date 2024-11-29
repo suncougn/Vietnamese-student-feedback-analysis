65bb94bcfadd57d8368e14017b86873f18d84540
Cường <phanc1777@gmail.com>
2024-11-29 13:53:49 +0700

    switch

--- build_model/build_rnn.py
+++ build_model/build_rnn.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class RNN(nn.Module):
-	def __init__(self, vocab_size, embedding_dim, rnn_units=64, num_layers=2, dropout=0.2, activation=None, batch_normalization= False, bidirectional=False):
+	def __init__(self, vocab_size, embedding_dim, rnn_units=64, num_layers=2, dropout=0.2, activation=None, batch_normalization= False, bidirectional=False, output_dim=3):
 		super(RNN, self).__init__()
 		self.embedding=nn.Embedding(vocab_size, embedding_dim)
 		self.rnn=nn.RNN(embedding_dim, rnn_units, num_layers, dropout=dropout, bidirectional=bidirectional)
@@ -14,7 +14,7 @@ class RNN(nn.Module):
 		self.bn=nn.BatchNorm1d(rnn_units) if batch_normalization else nn.Identity()
 		self.activation=activation
 		self.dropout=nn.Dropout(dropout)
-		self.dense2=nn.Linear(rnn_units,3)
+		self.dense2=nn.Linear(rnn_units, output_dim)
 	def forward(self, x):
 		x=self.embedding(x)
 		x, _ = self.rnn(x)
